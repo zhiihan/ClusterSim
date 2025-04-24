1980e242e49706c50c68b0cf790cd1d7d31ea2c1
Zhi Han <zhihuahan72@gmail.com>
2025-04-24 12:02:55 -0700

    update for heroku
    update for heroku again
    
    update for heroku3
    
    update for heroku

--- grid3dfigure.py
+++ grid3dfigure.py
@@ -35,7 +35,6 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server  # For deployment
 
-
 app.layout = html.Div(
     [
         PanelGroup(
