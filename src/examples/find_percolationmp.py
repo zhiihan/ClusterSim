657be362f7e3468e25ed1432c658eea01629b833
Zhi Han <zhihuahan72@gmail.com>
2025-04-24 11:24:25 -0700

    add black

--- src/examples/find_percolationmp.py
+++ src/examples/find_percolationmp.py
@@ -1,9 +1,9 @@
-from holes import Holes 
+from cluster_sim.app.holes import Holes
 import numpy as np
-from helperfunctions import *
 import pickle
 import networkx as nx
 import multiprocessing as mp
+import time
 
 import matplotlib.pyplot as plt
 
@@ -11,9 +11,11 @@ shape = [20, 20, 20]
 samples = 2
 p_vec = np.linspace(0.0, 1, 100)
 plot_data = np.empty((len(p_vec), 2))
-import multiprocessing as mp
-import time
+
+
 cpu_cores = 2
+
+
 # original code
 def main(input_vec):
     rounds = input_vec
@@ -23,7 +25,10 @@ def main(input_vec):
         sample_vec = np.zeros(samples)
 
         for seed in range(samples):
-            with open(f'./data/cc{p:.4f}shape{shape[0]}{shape[1]}{shape[2]}sample{seed}round{rounds}', 'rb') as f:
+            with open(
+                f"./data/cc{p:.4f}shape{shape[0]}{shape[1]}{shape[2]}sample{seed}round{rounds}",
+                "rb",
+            ) as f:
                 try:
                     cc = pickle.load(f)
                     low = np.array([np.inf, np.inf, np.inf])
@@ -31,25 +36,26 @@ def main(input_vec):
                     for n in cc:
                         low = np.minimum(low, np.array(n))
                         high = np.maximum(high, n)
-                    percol_dist = high[0]-low[0]
+                    percol_dist = high[0] - low[0]
 
                     sample_vec[seed] = percol_dist
-                    
+
                     if percol_dist >= shape[0] - 3:
-                        print(f'dist={percol_dist}, p={p}, seed={seed}, percolates, round={rounds}')
+                        print(
+                            f"dist={percol_dist}, p={p}, seed={seed}, percolates, round={rounds}"
+                        )
                     else:
-                        print(percol_dist, p, seed, 'does not percolate')
+                        print(percol_dist, p, seed, "does not percolate")
                 except EOFError:
-                    print('skipping', p, seed)
+                    print("skipping", p, seed)
                 except IndexError:
-                    print('skipping', p, seed)
-        
+                    print("skipping", p, seed)
+
         sample_vec = np.nan_to_num(sample_vec, neginf=0).astype(int)
         print(sample_vec)
         plot_data[p_index, 0] = p
         plot_data[p_index, 1] = np.mean(sample_vec)
     return (rounds, plot_data)
-
 
 
 if __name__ == "__main__":
@@ -62,13 +68,21 @@ if __name__ == "__main__":
 
     for i in results:
         rounds, plot_data = i
-        #plt.figure()
-        plt.title('x_max - x_min in the largest connected subgraph')
-        plt.xlabel('p, probability of losing a node')
-        plt.ylabel('x_max - x_min')
+        # plt.figure()
+        plt.title("x_max - x_min in the largest connected subgraph")
+        plt.xlabel("p, probability of losing a node")
+        plt.ylabel("x_max - x_min")
         if rounds < 5:
-            plt.plot(plot_data[:, 0], plot_data[:, 1], label=f'r = {rounds+1} rounds, shape = {shape}')
+            plt.plot(
+                plot_data[:, 0],
+                plot_data[:, 1],
+                label=f"r = {rounds+1} rounds, shape = {shape}",
+            )
         else:
-            plt.plot(plot_data[:, 0], plot_data[:, 1], label=f'r = {rounds} rounds, shape = {shape}')
+            plt.plot(
+                plot_data[:, 0],
+                plot_data[:, 1],
+                label=f"r = {rounds} rounds, shape = {shape}",
+            )
         plt.legend()
-    plt.savefig(f'percol{shape[0]}round{rounds}combined.png')
+    plt.savefig(f"percol{shape[0]}round{rounds}combined.png")
