9274c1d1599d480ea208efc41ef0c8588ac18233
Zhi Han <zhihuahan72@gmail.com>
2025-05-06 19:52:47 -0700

    add css to github

--- src/grid3dfigure.py
+++ src/grid3dfigure.py
@@ -23,7 +23,7 @@ app = dash.Dash(
     __name__,
     external_stylesheets=[
         dbc.themes.BOOTSTRAP,
-        "https://cdn.jsdelivr.net/gh/tcbegley/dash-bootstrap-css/dist/bootstrap/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/gh/zhiihan/cluster-sim/src/assets/bootstrap.min.css",
     ],
 )
 
