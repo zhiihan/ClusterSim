a35d37d6e09c5d58cb26b5e3a6b4dfe2ccdfe28a
Zhi Han <zhihuahan72@gmail.com>
2025-05-13 17:46:04 -0700

    download text and stab formatting

--- src/components/stabilizer.py
+++ src/components/stabilizer.py
@@ -27,6 +27,15 @@ Click on points in the graph. Can be copied to clipboard to load a graph state.
                         color="primary",
                         className="me-1",
                     ),
+                    dbc.Button(
+                        "Download Text",
+                        id="download-stab-btn",
+                        color="primary",
+                        className="me-1",
+                    ),
+                    dcc.Download(
+                        id="download-stab",
+                    ),
                     dcc.Clipboard(
                         target_id="stabilizer-data",
                         style={
@@ -35,7 +44,12 @@ Click on points in the graph. Can be copied to clipboard to load a graph state.
                     ),
                     html.Pre(
                         id="stabilizer-data",
-                        style={"border": "thin lightgrey solid", "overflowX": "scroll"},
+                        style={
+                            "border": "thin lightgrey solid",
+                            "white-space": "pre",
+                            "overflow": "auto",
+                            "word-wrap": "normal",
+                        },
                     ),
                 ],
                 style={
@@ -49,25 +63,54 @@ Click on points in the graph. Can be copied to clipboard to load a graph state.
 
 
 @callback(
-    Output("stabilizer-data", "children"),
-    Input("compute-stabilizers", "n_clicks"),
-    State("graph-data", "data"),
+    Output("download-stab", "data"),
+    Input("download-stab-btn", "n_clicks"),
     State("browser-data", "data"),
+    State("graph-data", "data"),
     prevent_initial_call=True,
 )
-def stabilizer_data(n_clicks, graphData, browserData):
+def download_stab(n_clicks, browserData, graphData):
+    s = jsonpickle.decode(browserData)
+    G = Grid(s.shape, json_data=graphData)
+    G.graph.remove_nodes_from(list(nx.isolates(G.graph)))
+    adjacency_matrix = nx.to_numpy_array(G.graph).astype(int)
+    adjacency_matrix += np.diag(2 * np.ones(adjacency_matrix.shape[0])).astype(int)
+
+    stabs = adjacency_mat_to_stabilizer(adjacency_matrix, newline=False)
+
+    return dict(content=stabs, filename="stabilizers.txt")
+
+
+@callback(
+    Output("stabilizer-data", "children"),
+    Output("browser-data", "data", allow_duplicate=True),
+    Input("compute-stabilizers", "n_clicks"),
+    State("browser-data", "data"),
+    State("graph-data", "data"),
+    prevent_initial_call=True,
+)
+def stabilizer_data(n_clicks, browserData, graphData):
     """
     Updates stabilizer data.
     """
 
     s = jsonpickle.decode(browserData)
     G = Grid(s.shape, json_data=graphData)
-    graph = G.graph.remove_nodes_from(list(nx.isolates(G.graph)))
+    G.graph.remove_nodes_from(list(nx.isolates(G.graph)))
 
     adjacency_matrix = nx.to_numpy_array(G.graph).astype(int)
 
     adjacency_matrix += np.diag(2 * np.ones(adjacency_matrix.shape[0])).astype(int)
 
+    s.stabilizer = adjacency_mat_to_stabilizer(adjacency_matrix)
+
+    return html.P(s.stabilizer), jsonpickle.encode(s)
+
+
+def adjacency_mat_to_stabilizer(adjacency_matrix, newline=True):
+    """
+    Convert the adjacency matrix to a stabilizer string.
+    """
     program = ""
 
     for index, rows in enumerate(adjacency_matrix):
@@ -77,13 +120,11 @@ def stabilizer_data(n_clicks, graphData, browserData):
         instruction = instruction.replace("[", "").replace("]", "").replace(" ", "")
 
         program += instruction
-        program += "\n"
 
-    print(program)
-
-    s.stabilizer = program
-
-    return html.P(s.stabilizer)
+        program += ","
+        if newline:
+            program += "\n"
+    return program
 
 
 def array2pauli(x):
