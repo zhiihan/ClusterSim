0749c221b4fedc3b04c8c586acbfe2685ebfffb6
Zhihua Han <zhihuahan72@gmail.com>
2026-02-22 15:14:33 +0100

    feat: add initial testing for rustworkx backend

--- src/cluster_sim/simulator/cluster_state.py
+++ src/cluster_sim/simulator/cluster_state.py
@@ -1,33 +1,39 @@
-from cluster_sim.graph_state import GraphState
-import networkx as nx
+from rustworkx.visualization import mpl_draw
+from graphsim import GraphRegister
+import rustworkx as rx
 
 
 class ClusterState:
     """
-    Adapter class containing a GraphState object.
+    Adapter class containing a GraphRegister object.
     """
 
-    def __init__(self, graph: nx.Graph):
+    def __init__(self, num_nodes: int):
         """
         Nodes are indexed from 0 to n-1, where n is the number of nodes in the graph.
 
-        Attributes:
-            graph: A networkx graph object.
-            graph_state: A GraphState object.
+        Args:
+            graph (rx.PyGraph | rx.PyDiGraph): A rustworkx graph object.
+            init_graph_state (bool, optional): Whether to auto apply H and CZ. Defaults to False.
         """
 
-        self.graph_state = GraphState(len(graph.nodes))
+        self.num_nodes = num_nodes
+        self.graph_state = GraphRegister(self.num_nodes)
 
-        if any(not isinstance(n, int) for n in graph.nodes):
-            self.graph = nx.convert_node_labels_to_integers(graph)
-        else:
-            self.graph = graph
+        # if any(not isinstance(n, int) for n in graph.nodes):
+        #     self.graph = rx.convert_node_labels_to_integers(graph)
+        # else:
+        #     self.graph = graph
 
-        for i in self.graph.nodes:
-            self.graph_state.h(i)
+    def __repr__(self):
+        rep = "Adjacency List:\n"
+        rep += self.graph_state.print_adjacency_list()
+        rep += "Stabilizers:\n"
+        rep += self.graph_state.print_stabilizers()
+        return rep
 
-        for e in self.graph.edges:
-            self.graph_state.add_edge(*e)
+    def __str__(self):
+        return self.graph_state.print_stabilizers()
 
     def measure(self, qubit: int, basis: str):
         """
@@ -37,38 +43,45 @@ class ClusterState:
             qubit: The qubit to measure, which is an integer from 0 to n-1.
             basis: The basis to measure in, which is either 'X', 'Y' or 'Z'.
         """
-        self.graph_state.measure(qubit, basis=basis)
-        self.graph = self.graph_state.to_networkx()
 
-    def x(self, qubit: int):
-        self.graph_state.x(qubit)
+        if basis == "Z":
+            pass
+        elif basis == "X":
+            self.H(qubit)
+        elif basis == "Y":
+            self.S_DAG(qubit)
+            self.H(qubit)
 
-    def y(self, qubit: int):
-        self.graph_state.y(qubit)
+        return self.graph_state.measure(qubit)
 
-    def z(self, qubit: int):
-        self.graph_state.z(qubit)
+    def X(self, qubit: int):
+        self.graph_state.X(qubit)
 
-    def h(self, qubit: int):
-        self.graph_state.h(qubit)
+    def Y(self, qubit: int):
+        self.graph_state.Y(qubit)
 
-    def s(self, qubit: int):
-        self.graph_state.s(qubit)
+    def Z(self, qubit: int):
+        self.graph_state.Z(qubit)
 
-    def s_dagger(self, qubit: int):
-        self.graph_state.s_dagger(qubit)
+    def H(self, qubit: int):
+        self.graph_state.H(qubit)
 
-    def cx(self, control: int, qubit: int):
-        self.graph_state.cx(control, qubit)
+    def S(self, qubit: int):
+        self.graph_state.S(qubit)
 
-    def cz(self, control: int, qubit: int):
-        self.graph_state.cz(control, qubit)
+    def S_DAG(self, qubit: int):
+        self.graph_state.S(qubit)
+        self.graph_state.S(qubit)
+        self.graph_state.S(qubit)
 
-    def sync_graph(self):
-        self.graph = self.graph_state.to_networkx()
-        self.graph.remove_nodes_from(list(nx.isolates(self.graph)))
+    def CX(self, control: int, qubit: int):
+        self.graph_state.CX(control, qubit)
 
-    def lc(self, qubit):
+    def CZ(self, control: int, qubit: int):
+        self.graph_state.CZ(control, qubit)
+
+    def LC(self, qubit):
+        raise NotImplementedError
         self.graph_state.local_complementation(qubit)
         self.graph = self.graph_state.to_networkx()
 
@@ -80,7 +93,11 @@ class ClusterState:
         Returns:
             A JSON-serializable representation of the graph state.
         """
-        return cls(nx.node_link_graph(json_data, edges="edges"))
+
+        cls.from_rustworkx(rx.parse_node_link_json(json_data))
+        cls._sync_graph()
+
+        return cls
 
     def to_json(self):
         """
@@ -89,44 +106,81 @@ class ClusterState:
         Returns:
             A JSON-serializable representation of the graph state.
         """
-        return nx.node_link_data(self.graph, edges="edges")
+        return rx.node_link_json(self.to_rustworkx(), node_attrs= lambda node: node)
 
+    @property
+    def stabilizers(self):
+        return self.graph_state.stabilizer_list()
 
-class NetworkXState:
-    """
-    Adapter class containing a NetworkX graph object.
-    """
+    @property
+    def adjacency_matrix(self):
+        return self.graph_state.adjacency_matrix()
 
-    def __init__(self, graph: nx.Graph):
+    @property
+    def adjacency_list(self):
+        return self.graph_state.adjacency_list()
+
+    @property
+    def vertex_operators(self):
+        return self.graph_state.vop_list()
+
+    def _sync_graph(self):
+        """This function should apply the VOPs to a graph state.
         """
-        Nodes are indexed from 0 to n-1, where n is the number of nodes in the graph.
 
-        Attributes:
-            graph: A networkx graph object.
+        for vop in self.vertex_operators:
+            pass
+            self.graph_state
+
+        raise NotImplementedError
+
+    def to_rustworkx(self):
+        """Export data from the underlying state.
+
+        Returns:
+            rx.PyGraph: rustworkx representation
         """
-        self.graph = graph
 
-    def sync_graph(self):
-        pass
+        g = rx.PyGraph(multigraph=False)
 
-    def add_node(self, i):
-        self.graph.add_node(i)
+        g.add_nodes_from(range(self.num_nodes))
+
+        g.add_edges_from([edge for edge in self._edge_list_from_adjacency_list()])
+
+        for node_index in g.node_indices():
+            g[node_index] = {
+                "stabilizer": self.stabilizers[node_index],
+                "vop": self.vertex_operators[node_index],
+            }
+
+        return g
+
+    def _edge_list_from_adjacency_list(self):
+        for u, neighbors in enumerate(self.adjacency_list):
+            for v in neighbors:
+                if u < v:
+                    yield (u, v, None)
 
     @classmethod
-    def from_json(cls, json_data):
-        """
-        Create a NetworkXState object from JSON data.
+    def from_rustworkx(cls, graph: rx.PyGraph | rx.PyDiGraph):
+        """Create a graph state from rustworkx.
+
+        Args:
+            graph (rx.PyGraph | rx.PyDiGraph): rustworkx graph.
 
         Returns:
-            A NetworkXState object.
+            ClusterState : class object
         """
-        return cls(nx.node_link_graph(json_data, edges="edges"))
+        c = cls(graph.num_nodes())
+        for i in graph.node_indices():
+            c.H(i)
 
-    def to_json(self):
-        """
-        Convert the graph state to a JSON-serializable format.
+        for e in graph.edge_list():
+            c.CZ(e[0], e[1])
 
-        Returns:
-            A JSON-serializable representation of the graph state.
-        """
-        return nx.node_link_data(self.graph, edges="edges")
+        return c
+
+    def draw(self, label_func = lambda node: str(node), **kwargs):
+        g = self.to_rustworkx()
+
+        mpl_draw(g, with_labels=True, labels=label_func, **kwargs)
--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,5 +1,6 @@
 import pytest
-from cluster_sim.graph_state import GraphState
+from cluster_sim.simulator import ClusterState
+import rustworkx as rx
 
 
 def test_apply_gate():
@@ -7,11 +8,21 @@ def test_apply_gate():
     Test measurement.
     """
 
-    G = GraphState(5)
+    g = rx.PyGraph()
+    g.add_nodes_from([0, 1, 2, 3, 4])
+
+    G = ClusterState(5)
 
     for i in range(5):
-        G.x(i)
+        G.X(i)
 
     for i in range(5):
         outcome = G.measure(i, basis="Z")
         assert outcome == 1, f"Measurement outcome {outcome} is not valid."
+
+
+def test_linear_graph():
+    g = rx.generators.path_graph(5)
+    G = ClusterState.from_rustworkx(g)
+
+    assert G.stabilizers == ["+XZIII", "+ZXZII", "+IZXZI", "+IIZXZ", "+IIIZX"]
